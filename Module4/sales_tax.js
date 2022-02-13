077c8489cb2e29933dd4ae370ea7a7c483eae65d
jtphillips215 <jtphillips215@gmail.com>
2022-02-13 16:08:38 -0600

    corrected errors with event handlers

--- Module4/sales_tax.js
+++ Module4/sales_tax.js
@@ -16,7 +16,7 @@ const processEntries = () => {
 
 // function for focusing on subtotal for page start and on clicking calculate
 const subtotalFocus = () => {
-  document.querySelector("#subTotal").focus();
+  $("#subtotal").focus();
 }
 
 window.onload = () => {
@@ -34,20 +34,24 @@ const clearForm = () => {
 
 // DOMContentLoaded event handler for attaching the processEntries() function
 // to the click event of the Calculate button
-let calculate = $("#calculate");
+document.addEventListener("DOMContentLoaded", () => {
+  let calculate = $("#calculate");
 calculate.addEventListener("click", processEntries);
+});
 
 // data validation for the processEntries() function
 // Subtotal must be > 0 and < 10000
 // Tax Rate must be > 0 and < 12
 
 // event handler for the click event of subtotal text bock clear the data from the text box
-let clearSubtotal = $("subtotal");
-clearSubtotal.addEventListener("click", $("#subtotal").value = "");
+$("#subtotal").addEventListener("click", () => {
+   $("#subtotal").value = "";
+});
 
 // event handler for the click event of tax rate text bock clear the data from the text box
-let clearTaxRate = $("subtotal");
-clearTaxRate.addEventListener("click", $("#tax_rate").value = "");
+$("#tax_rate").addEventListener("click", () => {
+  $("#tax_rate").value = "";
+});
 
 // Update the event handler for the Clear button to call these functions as well
 // (to reduce code duplication)
