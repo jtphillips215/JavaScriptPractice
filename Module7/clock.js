5b72bff430e43c5312612f20d21e8c792e50dedf
jtphillips215 <jtphillips215@gmail.com>
2022-03-21 18:17:40 -0500

    declaring variables for code

--- Module7/clock.js
+++ Module7/clock.js
@@ -4,6 +4,18 @@ const $ = selector => document.querySelector(selector);
 const padSingleDigit = num => num.toString().padStart(2, "0");
 
 const displayCurrentTime = () => {
+    // function makes a new date object, gets the current time, and updates the ui
+
+    // new date object for holding datetime
+    const currentDate = new Date();
+
+    // using date methods similar to those from ch4 to set variables to current system datetime
+    const currentHours = currentDate.getHours();
+    const currentMinutes = currentDate.getMinutes();
+    const currentSeconds = currentDate.getSeconds();
+    const ampm = "AM";
+
+
 
 };
 
