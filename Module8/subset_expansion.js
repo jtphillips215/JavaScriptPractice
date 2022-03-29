f59881b41d04108a64427e41d6f649547b8c9095
jtphillips215 <jtphillips215@gmail.com>
2022-03-28 19:38:22 -0500

    show more working now

--- Module8/subset_expansion.js
+++ Module8/subset_expansion.js
@@ -1,10 +1,21 @@
-"use strict";
 $(document).ready(function() {
 
   // attaching second function for the click event of the a tags/links
   $("#jdom a").click( evt => {
     
     // current target determines which like is clicked
-    const clickedLink = evt.currentTarget;
+    const currentLink = evt.currentTarget;
+    
+    // toggle minus class for a tag 
+    $(currentLink).toggleClass("hide");
+    
+    // show or hide related div 
+    if ($(currentLink).prev().attr("class") != "hide") {
+      $(currentLink).prev().hide();
+    } 
+    else { $(currentLink).prev().show(); }
+    
+    // preventiong default behavior for link
+    evt.preventDefault();
   }); 
 });
