3ecfb15642552ed3b98ef4ccb92a9aa02547ccb2
jtphillips215 <jtphillips215@gmail.com>
2022-04-17 12:56:11 -0500

    first part with 1dash at beginning

--- Module10/regex.js
+++ Module10/regex.js
@@ -3,7 +3,12 @@
 $(document).ready( () => {
     $("#validate").click( () => {
         const phone = $("#phone").val();
-        const pattern = /^\d{3}-\d{3}-\d{4}$/;   // 999-999-9999
+        // initial pattern
+        // const pattern = /^\d{3}-\d{3}-\d{4}$/;    //999-999-9999
+        // adding prefix for 1
+        // const pattern = /^\d{1}-\d{3}-\d{3}-\d{4}$/;    //1-555-555-5555
+        //optional prefic for 1
+        const pattern = /^\d{3}-\d{3}-\d{4}$/
         const isValid = pattern.test(phone);
 
         $("#message").text( (isValid) ? "Valid phone number" : "Invalid phone number" );
