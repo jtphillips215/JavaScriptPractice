8a3acc362ad6405a9c8ef2faadebbfa66b564a1c
jtphillips215 <jtphillips215@gmail.com>
2022-04-24 11:13:38 -0500

    added delete cookie function, began working on set cookie function

--- Module11/login.js
+++ Module11/login.js
@@ -5,11 +5,22 @@ const getCookieByName = name => {
 };
 
 const setCookie = (name, value, days) => {
-
+    // creating our name value pair
+    let cookie = name + "=" + encodeURIComponent(value);
+    // setting max age
+    if (days) {
+        cookie += "; max-age=" + days * 24 * 60 * 60;
+    }
+    cookie += "; path=/";
+    // setting cookie in doc
+    document.cookie = cookie;
+    // testing our cookie
+    document.write(cookie);
 };
 
 const deleteCookie = name => {
-
+    // deleting our cookie
+    document.cookie = name + "=''; max-age=0; path=/"; 
 };
 
 const goToPage = url => {
