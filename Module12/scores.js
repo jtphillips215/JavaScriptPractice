a0cfd632ce026fd580d058e8fe9d669f8af02d02
jtphillips215 <jtphillips215@gmail.com>
2022-04-28 21:18:36 -0500

    averages and average text set

--- Module12/scores.js
+++ Module12/scores.js
@@ -5,12 +5,31 @@ const scores = new Array();
 const scoresString = new Array();
 
 const displayScores = (scores, scoresString) => {
-	
-};
+    //console.log(scores);
+    
+    // total that can be added to as we iterate through array
+    let total = 0;
+    // iterating through array and adding to total 
+	for (let i = 0; i < scores.length; i++) {
+        let score = parseInt(scores[i]); 
+        total += score;
+        //console.log(total);
+    }
+
+    // calculating average and updating blank
+    let average = total / scores.length;
+    console.log(average);
+    $("#average_score").text(average);
+}
 
 $(document).ready( () => {
     $("#add_button").click( () => {
-        
+        // adding our score to the scores array
+        let score = $("#score").val();
+        //console.log(score);
+        scores[scores.length] = score;
+        // calling display scores to update with the required information
+        displayScores(scores, scoresString);
         
         // get the add form ready for next entry
         $("#first_name").val( "" );
